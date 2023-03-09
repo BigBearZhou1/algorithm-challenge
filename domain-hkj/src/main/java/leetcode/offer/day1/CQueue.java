5b7ee5cc126453fe1675e08762ad0c4bb082790a
kuangjiHU <1317465126@qq.com>
2023-03-10 00:16:55 +0800

    2023-03-09 week-01 hkj 用两个栈实现队列

---
+++ domain-hkj/src/main/java/leetcode/offer/day1/CQueue.java
@@ -0,0 +1,32 @@
+package leetcode.offer.day1;
+
+import java.util.Stack;
+
+public class CQueue {
+
+    private final Stack<Integer> stackTail;
+    private final Stack<Integer> stackHead;
+
+    public CQueue() {
+        stackTail = new Stack<>();
+        stackHead = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        stackTail.push(value);
+    }
+
+    public int deleteHead() {
+
+        if (!stackHead.isEmpty()) {
+            return stackHead.pop();
+        } else {
+            while (!stackTail.isEmpty()) {
+                stackHead.push(stackTail.pop());
+            }
+        }
+        return stackHead.isEmpty() ? -1 : stackHead.pop();
+    }
+
+}
+
