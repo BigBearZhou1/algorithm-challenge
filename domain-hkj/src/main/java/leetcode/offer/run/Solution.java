bf81089e528fe983f49e8367e8313292be758f9a
kuangjiHU <1317465126@qq.com>
2023-03-10 00:43:25 +0800

    2023-03-08 hkj week-01 用两个栈实现队列

--- domain-hkj/src/main/java/leetcode/offer/run/Solution.java
+++ domain-hkj/src/main/java/leetcode/offer/run/Solution.java
@@ -4,6 +4,11 @@ import leetcode.offer.day1.CQueue;
 
 public class Solution {
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.runDay01();
+    }
+
+    private void runDay01(){
         CQueue queue = new CQueue();
         queue.appendTail(1);
         queue.appendTail(2);
@@ -11,4 +16,5 @@ public class Solution {
         queue.appendTail(4);
         System.out.println(queue.deleteHead());
     }
+
 }
