1d9e2ac0485b93c69616745321f2d99c9ba71057
kuangjiHU <1317465126@qq.com>
2023-03-13 23:23:54 +0800

    2023-03-13 hkj week-01 包含min函数的栈

--- domain-hkj/src/main/java/leetcode/offer/run/Solution.java
+++ domain-hkj/src/main/java/leetcode/offer/run/Solution.java
@@ -1,11 +1,13 @@
 package leetcode.offer.run;
 
 import leetcode.offer.day1.CQueue;
+import leetcode.offer.day2.MinStack;
 
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.runDay01();
+//        solution.runDay01();
+        solution.runDay02();
     }
 
     private void runDay01(){
@@ -15,6 +17,19 @@ public class Solution {
         queue.appendTail(3);
         queue.appendTail(4);
         System.out.println(queue.deleteHead());
+    }
+
+    private void runDay02(){
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.min());
+        minStack.pop();
+        System.out.println(minStack.top());
+        minStack.min();
+        System.out.println(minStack.min());
+
     }
 
 }
