0ddace5899f2184de022c185e38a673eaee4e93a
kuangjiHU <1317465126@qq.com>
2023-03-17 23:12:57 +0800

    2023-03-17 hkj week-02 替换字符串中的空格

--- domain-hkj/src/main/java/leetcode/offer/day6/StringWithoutSpace.java
+++ domain-hkj/src/main/java/leetcode/offer/day6/StringWithoutSpace.java
@@ -1,2 +1,16 @@
-package leetcode.offer.day6;public class StringWithoutSpace {
+package leetcode.offer.day6;
+
+public class StringWithoutSpace {
+
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (' ' == c) {
+                sb.append("%20");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
 }
