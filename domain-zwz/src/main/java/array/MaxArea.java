d25b69e32a61650df566934fd0251bd0d433ef93
BigBearZhou1 <zhouwenzong@126.com>
2023-03-24 15:13:33 +0800

    2023-03-24 Fri zwz Week03-01 盛水最多的容器 双指针

---
+++ domain-zwz/src/main/java/array/MaxArea.java
@@ -0,0 +1,17 @@
+package array;
+
+public class MaxArea {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1, max = 0;
+        while (l != r) {
+            int cur = Math.min(height[l], height[r]) * (r - l);
+            max = Math.max(max,cur);
+            if(height[l]<=height[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return max;
+    }
+}
