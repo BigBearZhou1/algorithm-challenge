9c008dd6501a7d70bdfc15409cf72f7967ddf39d
kuangjiHU <1317465126@qq.com>
2023-04-04 22:57:27 +0800

    2023-03-08 hkj week-04 镜像二叉树

--- domain-hkj/src/main/java/leetcode/offer/week4/day02/SubStructure.java
+++ domain-hkj/src/main/java/leetcode/offer/week4/day02/SubStructure.java
@@ -16,11 +16,13 @@ class TreeNode {
 public class SubStructure {
 
     public boolean isSubStructure(TreeNode A, TreeNode B) {
-        return (A != null && B != null) && (isSameTree(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+        if (A == null || B == null) return false;
+        return isSubTree(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
     }
-    boolean isSameTree(TreeNode A, TreeNode B) {
-        if(B == null) return true;
-        if(A == null || A.val != B.val) return false;
-        return isSameTree(A.left, B.left) && isSameTree(A.right, B.right);
+
+    boolean isSubTree(TreeNode A, TreeNode B) {
+        if (B == null) return true;
+        if (A == null || A.val != B.val) return false;
+        return isSubTree(A.left, B.left) && isSubTree(A.right, B.right);
     }
 }
---
+++ domain-hkj/src/main/java/leetcode/offer/week4/day03/TreeMirror.java
@@ -0,0 +1,30 @@
+package leetcode.offer.week4.day03;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class TreeMirror {
+    public TreeNode mirrorTree(TreeNode root) {
+        swap(root);
+        return root;
+    }
+
+    public void swap(TreeNode root) {
+
+        if (null == root) return;
+        TreeNode tempTree = root.left;
+
+        root.left = root.right;
+        root.right = tempTree;
+        swap(root.left);
+        swap(root.right);
+    }
+
+}
